c8aff3897023f31ec3880b43ffa60692da8bacee
reimiii <imiia75775@gmail.com>
2023-01-01 21:04:09 +0700

    tipe data character

---
+++ src/Char.java
@@ -0,0 +1,11 @@
+public class Char {
+    public static void main(String[] args) {
+        char i = 'I';
+        char m = 'M';
+        char i2 = 'I';
+
+        System.out.println(i);
+        System.out.println(m);
+        System.out.println(i2);
+    }
+}
