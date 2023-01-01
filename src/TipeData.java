21d0910a7f365b4e49050bbf5bff46e662b56f0c
reimiii <imiia75775@gmail.com>
2023-01-01 20:37:24 +0700

    tipe data number

---
+++ src/TipeData.java
@@ -0,0 +1,22 @@
+public class TipeData {
+    public static void main(String[] args) {
+
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInteger = 1000000;
+        long iniLong = 1000000000;
+        long iniLong2 = 100000000L;
+
+        float iniFloat = 10.12F;
+        double iniDouble = 10.10;
+
+        int decimalInt = 25;
+        int hexInt = 0xA132B;
+        int binInt = 0b01010011;
+
+        long balance = 1_000_000_000;
+        int sum = 60_000_00;
+
+
+    }
+}
