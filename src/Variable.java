335dc3e02ae49156d91170eb63813d05cfd903b9
reimiii <imiia75775@gmail.com>
2023-01-22 00:46:41 +0700

    Variable

---
+++ src/Variable.java
@@ -0,0 +1,26 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "Hilmi Akbar Muharrom";
+
+        int age = 18;
+
+        String address;
+        address = "Indonesia";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(address);
+
+        name = "Mika";
+        System.out.println(name);
+
+        var namaKu = "Nibbbbbes";
+        var alamatKu = "Bogor";
+
+        final String theName = "Hilmi Akbar Muharrom";
+
+//        theName = "okee";
+
+    }
+}
