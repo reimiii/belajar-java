c39dba7003e0aa606506b8304f87223f3bd6abf5
reimiii <imiia75775@gmail.com>
2023-01-21 22:13:42 +0700

    tipe data String

---
+++ src/TipeDataString.java
@@ -0,0 +1,12 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firtName = "Hilmi";
+        String lastName = "AM";
+        String fullName = firtName + " " + lastName;
+
+        System.out.println(fullName);
+
+        System.out.println(firtName);
+        System.out.println(lastName);
+    }
+}
