f3bbe67fe22e850069ca7a4e3bb8ac84907777fe
reimiii <imiia75775@gmail.com>
2023-01-28 22:39:44 +0700

    Operator Perbandingan

---
+++ src/OperatorPerbandingan.java
@@ -0,0 +1,32 @@
+public class OperatorPerbandingan {
+    public static void main(String[] args) {
+        /*
+        * Operasi Perbandingan
+        * operasi untuk membandingkan dua buah data
+        * operasi yang menghasilkan nilai boolean true or false
+        * jika benar maka true
+        * jika salah make false
+        *
+        * Operator          Keterangan
+        * >                 Lebih Dari
+        * <                 Kurang Dari
+        * >=                Lebih Dari sama dengan
+        * <=                Kurang dari sama dengan
+        * ==                Sama dengan
+        * !=                Tidak sama dengan
+        *  */
+
+        int valOne = 100;
+        int valTwo = 100;
+
+        System.out.println(valOne > valTwo);
+        System.out.println(valOne < valTwo);
+        System.out.println(valOne >= valTwo);
+        System.out.println(valOne <= valTwo);
+        System.out.println(valOne == valTwo);
+        System.out.println(valOne != valTwo);
+
+
+
+    }
+}
