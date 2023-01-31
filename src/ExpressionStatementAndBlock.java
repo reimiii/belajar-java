f7fa40b1880d6e8eac04882287a6bdd41ce1c6b1
reimiii <imiia75775@gmail.com>
2023-01-31 13:37:16 +0700

    ExpressionStatementAndBlock

---
+++ src/ExpressionStatementAndBlock.java
@@ -0,0 +1,29 @@
+public class ExpressionStatementAndBlock {
+    /*
+    * Expression
+    * adalah kontruksi dari variable, operator dan pemanggilan method yang mengevaluasi menjadi
+    * sebuah single value
+    *
+    * adalah core component dari statment
+    * */
+    public static void main(String[] args) {
+
+        int value; // Expresi
+        value = 10; // Expresi
+
+        // ini statment    //ini expres
+        System.out.println(value = 100);
+
+        /*
+        * Statment
+        * Statment bisa di bilang adalah kalimat lengkap dalam bahasa
+        * sebuah statment berisikan execution komplit, biasanya diakhiri dengan titik koma
+        * ada beberapa jenis statment:
+        * Assignment expession
+        * Penggunaan ++ dan --
+        * Method invocation
+        * Object creation expression
+        * */
+
+    }
+}
