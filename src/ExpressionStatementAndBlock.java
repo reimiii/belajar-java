e86f63c07dabca0da3220c082e685791bf5c21dd
reimiii <imiia75775@gmail.com>
2023-01-31 13:43:16 +0700

    A I M O jenis statement

--- src/ExpressionStatementAndBlock.java
+++ src/ExpressionStatementAndBlock.java
@@ -1,11 +1,13 @@
+import java.util.Date;
+
 public class ExpressionStatementAndBlock {
     /*
-    * Expression
-    * adalah kontruksi dari variable, operator dan pemanggilan method yang mengevaluasi menjadi
-    * sebuah single value
-    *
-    * adalah core component dari statment
-    * */
+     * Expression
+     * adalah kontruksi dari variable, operator dan pemanggilan method yang mengevaluasi menjadi
+     * sebuah single value
+     *
+     * adalah core component dari statment
+     * */
     public static void main(String[] args) {
 
         int value; // Expresi
@@ -15,15 +17,27 @@ public class ExpressionStatementAndBlock {
         System.out.println(value = 100);
 
         /*
-        * Statment
-        * Statment bisa di bilang adalah kalimat lengkap dalam bahasa
-        * sebuah statment berisikan execution komplit, biasanya diakhiri dengan titik koma
-        * ada beberapa jenis statment:
-        * Assignment expession
-        * Penggunaan ++ dan --
-        * Method invocation
-        * Object creation expression
-        * */
+         * Statment
+         * Statment bisa di bilang adalah kalimat lengkap dalam bahasa
+         * sebuah statment berisikan execution komplit, biasanya diakhiri dengan titik koma
+         * ada beberapa jenis statment:
+         * Assignment expession
+         * Penggunaan ++ dan --
+         * Method invocation
+         * Object creation expression
+         * */
+
+        // Assignment expession
+        double avalue = 9998.122;
+
+        // increment statement
+        avalue++;
+
+        // method invocation statement
+        System.out.println("Hello World!");
+
+        // object creation statement
+        Date date = new Date();
 
     }
 }
