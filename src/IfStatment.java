b5d34713a63f0c93ca857be86ebc84e95544b47e
reimiii <imiia75775@gmail.com>
2023-02-06 23:09:10 +0700

    if Statement

---
+++ src/IfStatment.java
@@ -0,0 +1,27 @@
+public class IfStatment {
+    public static void main(String[] args) {
+        var nilai = 70;
+        var absen = 90;
+
+        var hasil = nilai >= 75 && absen >= 75;
+        System.out.println(hasil);
+
+        if (hasil) {
+            System.out.println("Anda Lulus");
+        } else {
+            System.out.println("Anda Kurang Beruntung");
+        }
+
+        if (nilai >= 80 && absen >= 80) {
+            System.out.println("Nilai A");
+        } else if (nilai >= 70 && absen >= 70) {
+            System.out.println("Nilai B");
+        } else if (nilai >= 60 && absen >= 60) {
+            System.out.println("Nilai C");
+        } else if (nilai >= 50 && absen >= 50) {
+            System.out.println("Nilai D");
+        } else {
+            System.out.println("Nilai E");
+        }
+    }
+}
