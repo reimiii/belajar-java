3b4894b2c926dd91b347ab6c282c40abef00da45
reimiii <imiia75775@gmail.com>
2023-02-11 20:45:05 +0700

    TernaryOperator

---
+++ src/TernaryOperator.java
@@ -0,0 +1,28 @@
+public class TernaryOperator {
+
+    /*
+    * Ternary operator adalah operator sederhana dari if statement
+    * Ternary operator terdiri dari  kondisi yang dievaluasi, jika menghasilkan true maka nilai
+    * pertama diambil, jika false, maka nilai kedua diambil
+    * */
+    public static void main(String[] args) {
+        // kode tampa ternary operator
+        var nilai = 75;
+        String ucapan;
+
+        if (nilai >= 75) {
+            ucapan = "Selamat Anda Lulus!!";
+            System.out.println(ucapan);
+        } else {
+            ucapan = "Kurang Beruntung";
+            System.out.println(ucapan);
+        }
+
+        // kode dengan ternary operator
+
+        ucapan = nilai >= 75 ? "Selamat Bang anda Lulus!!" : "Lu salah jurusan";
+        System.out.println(ucapan);
+    }
+
+
+}
