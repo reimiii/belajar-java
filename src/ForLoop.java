4795cb5eddd8ed18ff9610b65b12750edac0c9a0
reimiii <imiia75775@gmail.com>
2023-02-11 21:06:15 +0700

    ForLoop

---
+++ src/ForLoop.java
@@ -0,0 +1,19 @@
+public class ForLoop {
+    /*
+     * For adalah salah satu kata kunci yang bisa digunakan untuk melakukan perulangan
+     * Blok kode yang terdapat di dalam for akan selalu diulangi selama kondisi for terpenuhi
+     *
+     * sintak perulangan for
+     *
+     * for(init statement; kondisi; post statement){
+     *  // block perulangan
+     * }
+     *
+     * Init statement akan dieksekusi hanya sekali di awal sebelum perulangan
+     * kondisi akan di lakukan pengecekan dalam setiap perulangan, jika true
+     * perulangan akan dilakukan jika false perulangan akan berhenti
+     * */
+    public static void main(String[] args) {
+
+    }
+}
