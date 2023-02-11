bca4e2112927a82c64aa0b28ab0bf8a8a4595e04
reimiii <imiia75775@gmail.com>
2023-02-11 20:45:41 +0700

    TernaryOperatorV2

--- src/TernaryOperator.java
+++ src/TernaryOperator.java
@@ -1,10 +1,10 @@
 public class TernaryOperator {
 
     /*
-    * Ternary operator adalah operator sederhana dari if statement
-    * Ternary operator terdiri dari  kondisi yang dievaluasi, jika menghasilkan true maka nilai
-    * pertama diambil, jika false, maka nilai kedua diambil
-    * */
+     * Ternary operator adalah operator sederhana dari if statement
+     * Ternary operator terdiri dari  kondisi yang dievaluasi, jika menghasilkan true maka nilai
+     * pertama diambil, jika false, maka nilai kedua diambil
+     * */
     public static void main(String[] args) {
         // kode tampa ternary operator
         var nilai = 75;
