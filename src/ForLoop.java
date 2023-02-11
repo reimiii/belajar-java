3a98ccaa67b757f99fd0a8370b2d9cb520615655
reimiii <imiia75775@gmail.com>
2023-02-11 21:08:50 +0700

    ForLoopV1

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -12,6 +12,9 @@ public class ForLoop {
      * Init statement akan dieksekusi hanya sekali di awal sebelum perulangan
      * kondisi akan di lakukan pengecekan dalam setiap perulangan, jika true
      * perulangan akan dilakukan jika false perulangan akan berhenti
+     * Post statement akan dieksekusi setiap kali diakhir perulangan
+     * Init statement, Kondisi dan Post Statement tidak wajib diisi, jika Kondisi tidak diisi, berarti
+     * Kondisi selalu ber nilai true
      * */
     public static void main(String[] args) {
 
